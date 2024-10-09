b86e204e1dbcc217e30cfdfd6764cb23e6c7f378
Balaji <balaji0017@gmail.com>
2024-10-09 10:57:26 +0530

    Added LoginPage object

---
+++ EmployeeManagementAutomation/Pages/LoginPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementAutomation.Pages
+{
+    public class LoginPage
+    {
+        private By usernameLocator = By.Name("username");
+        private By passwordLocator = By.Name("password");
+        private By loginLocator = By.XPath("//button[contains(normalize-space(),'Login')]");
+        private By errorLocator = By.XPath("//p[contains(normalize-space(),'Invalid')]");
+
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void EnterUsername(string username)
+        {
+            driver.FindElement(usernameLocator).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            driver.FindElement(passwordLocator).SendKeys(password);
+        }
+
+        public void ClickOnLogin()
+        {
+            driver.FindElement(loginLocator).Click();
+        }
+
+        public string GetInvalidErrorMessage()
+        {
+            return driver.FindElement(errorLocator).Text;
+        }
+    }
+}
